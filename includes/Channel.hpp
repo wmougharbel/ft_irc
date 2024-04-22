e1d7b0eda13ccec9b3a790fee4a216221efcf2cf
Adam Murawski <murasmurawski@gmail.com>
2024-04-22 21:39:00 +0200

    changed vector<string> to vector<Client>

--- includes/Channel.hpp
+++ includes/Channel.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loandrad <loandrad@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: amurawsk <amurawsk@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:44:35 by walid             #+#    #+#             */
-/*   Updated: 2024/04/22 19:50:26 by loandrad         ###   ########.fr       */
+/*   Updated: 2024/04/22 21:38:14 by amurawsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,25 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include "../includes/Client.hpp"
 
 class Channel {
 private:
 	std::string name; 
-	std::vector<std::string> members;
-	std::string topic; 
-	bool isinviteOnly; 
-	bool istopicRestricted;
-	bool ispassword;
-	int limit;
-	std::string ChannelKey; 
-	std::vector <std::string> operators;
+
+	std::vector<Client>	_members;
+	std::vector<Client>	_operators;
+
+	std::string	_topic;
+	std::string	_ChannelKey; 
+
+	bool		_isinviteOnly;
+	bool		_istopicRestricted;
+	bool		_ispassword;
+	int			_limit;
 
 public:
-	Channel(const std::string& name,const std::string& firstMember);
+	Channel(const std::string& name,const Client& firstMember);
 	~Channel(); 
 
 	void setlimit(const int limit);
@@ -46,13 +50,12 @@ public:
 	void setChannelKey(const std::string& key);
 	bool checkChannelKey(const std::string& key) const;
 
-	void setOperatorPrivileges(const std::string &member);
+	void setOperatorPrivileges(const Client &member);
 	void removeOperatorPrivileges(const std::string &member);
 	bool hasOperatorPrivileges(const std::string &member) const;
 
 	void removeMember(const std::string& nickname);
 	bool isMember(const std::string& nickname) const;
-
 };
 
 #endif
--- sources/Channel.cpp
+++ sources/Channel.cpp
@@ -3,92 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loandrad <loandrad@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: amurawsk <amurawsk@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 18:45:03 by walid             #+#    #+#             */
-/*   Updated: 2024/04/22 19:32:52 by loandrad         ###   ########.fr       */
+/*   Updated: 2024/04/22 21:38:19 by amurawsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Channel.hpp"
 
-Channel::Channel(const std::string& name, const std::string& firstMember) : name(name), isinviteOnly(false), istopicRestricted(false), ispassword(false), limit(0) {
-	members.push_back(firstMember);
-	operators.push_back(firstMember);
+Channel::Channel(const std::string& name, const Client& firstMember) : name(name), _isinviteOnly(false), _istopicRestricted(false), _ispassword(false), _limit(-1){
+	_members.push_back(firstMember);
+	_operators.push_back(firstMember);
 }
 
 Channel::~Channel() {}
 
 void Channel::setlimit(const int limit){
-	this->limit = limit;
+	this->_limit = limit;
 }
 
 
 void Channel::setTopic(const std::string& topic) {
-	this->topic = topic;
+	this->_topic = topic;
 }
 
 std::string Channel::getTopic() const {
-	return topic;
+	return _topic;
 }
 
 void Channel::setTopicRestricted(bool topicRestricted) {
-	this->istopicRestricted = topicRestricted;
+	this->_istopicRestricted = topicRestricted;
 }
 
 bool Channel::isTopicRestricted() const {
-	return istopicRestricted;
+	return _istopicRestricted;
 }
 
 
 
 void Channel::setInviteOnly(bool inviteOnly) {
-	this->isinviteOnly = inviteOnly;
+	this->_isinviteOnly = inviteOnly;
 }
 
 bool Channel::isInviteOnly() const {
-	return isinviteOnly;
+	return _isinviteOnly;
 }
 
 
 
 void Channel::setChannelKey(const std::string& key) {
-	this->ChannelKey = key;
+	this->_ChannelKey = key;
 }
 
 bool Channel::checkChannelKey(const std::string& key) const {
-	return key == ChannelKey;
+	return key == _ChannelKey;
 }
 
-void Channel::setOperatorPrivileges(const std::string& member) {
-	operators.push_back(member);
+void Channel::setOperatorPrivileges(const Client& member) {
+	_operators.push_back(member);
 }
 
-void Channel::removeOperatorPrivileges(const std::string& member) {
-	if(hasOperatorPrivileges(member) == false){
-		std::cout << member << " is not an operator of this Channel!" << std::endl;
+void Channel::removeOperatorPrivileges(const std::string& nickname) {
+	if(hasOperatorPrivileges(nickname) == false){
+		std::cout << nickname << " is not an operator of this Channel!" << std::endl;
 		return;
 	}
-	for (std::vector<std::string>::iterator it = operators.begin(); it != operators.end(); ++it) {
-		if (*it == member) {
-			operators.erase(it);
+	for (std::vector<Client>::iterator it = _operators.begin(); it != _operators.end(); ++it) {
+		if (it->getNickname() == nickname) {
+			_operators.erase(it);
 			break;
 		}
 	}
-	if(operators.empty() && !members.empty()){
-		for (std::vector<std::string>::const_iterator it = members.begin(); it != members.end();++it) {
-			if (*it == member) 
+	if(_operators.empty() && !_members.empty()){
+		for (std::vector<Client>::const_iterator it = _members.begin(); it != _members.end();++it) {
+			if (it->getNickname() == nickname) 
 				continue;
-			setOperatorPrivileges(member);
+			setOperatorPrivileges(*it);
 		}
 	}
 }
 
-bool Channel::hasOperatorPrivileges(const std::string& member) const {
-	if(operators.empty())
+bool Channel::hasOperatorPrivileges(const std::string& nickname) const {
+	if(_operators.empty())
 		return false;
-	for (std::vector<std::string>::const_iterator it = operators.begin(); it != operators.end(); ++it) {
-		if (*it == member)
+	for (std::vector<Client>::const_iterator it = _operators.begin(); it != _operators.end(); ++it) {
+		if (it->getNickname() == nickname)
 			return true;
 	}
 	return false;
@@ -99,23 +99,23 @@ void Channel::removeMember(const std::string& nickname) {
 		std::cout << nickname << " is not a member of this Channel!" << std::endl;
 		return;
 	}
-	for (std::vector<std::string>::iterator it = members.begin(); it != members.end(); ++it) {
-		if (*it == nickname) {
-			members.erase(it);
+	for (std::vector<Client>::iterator it = _members.begin(); it != _members.end(); ++it) {
+		if (it->getNickname() == nickname) {
+			_members.erase(it);
 			break;
 		}
 	}
 	if(hasOperatorPrivileges(nickname))
 		removeOperatorPrivileges(nickname);
-	if(members.empty())
+	if(_members.empty())
 		delete this;
 }
 
 bool Channel::isMember(const std::string& nickname) const {
-	if(members.empty())
+	if(_members.empty())
 		return false;
-	for (std::vector<std::string>::const_iterator it = members.begin(); it != members.end(); ++it) {
-		if (*it == nickname) {
+	for (std::vector<Client>::const_iterator it = _members.begin(); it != _members.end(); ++it) {
+		if (it->getNickname() == nickname) {
 			return true;
 		}
 	}
